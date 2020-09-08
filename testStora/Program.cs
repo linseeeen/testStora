fe51b7eab3d01d45189d8bab8fc767e9f8834e4d
Linnea <linnea@klangby.com>
2020-09-08 10:28:05 +0200

    first

---
+++ testStora/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testStora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
